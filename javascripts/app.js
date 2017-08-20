92f81a2218520c39ea2cf6837e8e38879dfbfd96
Nethra <nethra12a@gmail.com>
2017-08-20 21:40:54 +0530

    Updated Gallery page with side tab link

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,4 @@
 ;(function ($, window, undefined) {
   'use strict';
   $("#featured").orbit();
-
-  $("#paint").click(function(){
-    $('#paintings').html('<iframe src="Paintings.html" frameborder="0" scrolling="no" id="myFrame"></iframe>');
-  })
 })(jQuery, this);
