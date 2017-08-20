82fdd0387342450e708b38eff87fcbf2b05b6877
Nethra <nethra12a@gmail.com>
2017-08-20 13:44:11 +0530

    iframe updates
    -Neths

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,8 @@
 ;(function ($, window, undefined) {
   'use strict';
   $("#featured").orbit();
+
+  $("#paint").click(function(){
+    $('#paintings').html('<iframe src="Paintings.html" frameborder="0" scrolling="no" id="myFrame"></iframe>');
+  })
 })(jQuery, this);
